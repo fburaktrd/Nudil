bfc3b834e43f8f95ef24e60812ab2c0188ef784a
fburaktrd <fburakturedi@hotmail.com>
2021-04-07 04:27:28 +0300

    User sign up and in added.
    Kullanıcı girişi ve kayıt olma eklendi.

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_first_app/services/auth.dart';
+
+class SignIn extends StatefulWidget {
+  final Function toggleView;
+  SignIn({this.toggleView});
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
+  // text field state
+  String email = '';
+  String password = '';
+  String error = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red[100],
+      appBar: AppBar(
+        backgroundColor: Colors.red[400],
+        elevation: 0.0,
+        title: Text('Sign in to our_app_name'),
+        actions: <Widget>[
+          FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('Register'),
+              onPressed: () {
+                widget.toggleView();
+              })
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              TextFormField(
+                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  }),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                  obscureText: true,
+                  validator: (val) =>
+                      val.length < 6 ? 'Enter a password 6+ chars long' : null,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  }),
+              SizedBox(
+                height: 20.0,
+              ),
+              RaisedButton(
+                color: Colors.pink[400],
+                child: Text(
+                  'Sign in',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _auth.signInWithEmailAndPassword(
+                        email, password);
+
+                    if (result == null) {
+                      setState(() => error = 'Could not sign in with those credentials.');
+                    }
+                  }
+                },
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nyp_proje/main.dart';
-
+import 'package:nyp_proje/lib/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
