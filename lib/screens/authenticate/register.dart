fcd9ca8628a94cc67746ca326b9694b041b84c39
fburaktrd <fburakturedi@hotmail.com>
2021-04-07 04:18:49 +0300

    User sign up and in added
    Kullanıcı girişi ve kayıt olma eklendi.

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_first_app/services/auth.dart';
+
+class Register extends StatefulWidget {
+  final Function toggleView;
+  Register({this.toggleView});
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
+  String error = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red[100],
+      appBar: AppBar(
+        backgroundColor: Colors.red[400],
+        elevation: 0.0,
+        title: Text('Sign up to our_app_name'),
+        actions: <Widget>[
+          FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('Sign In'),
+              onPressed: () {
+                widget.toggleView();
+              })
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              TextFormField(
+                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  }),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                  obscureText: true,
+                  validator: (val) => val.length < 6
+                      ? 'Enter the password 6+ chars long'
+                      : null,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  }),
+              SizedBox(
+                height: 20.0,
+              ),
+              RaisedButton(
+                color: Colors.pink[400],
+                child: Text(
+                  'Register',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _auth.registerWithEmailAndPassword(
+                        email, password);
+                    if (result == null) {
+                      setState(() => error = 'Please supply a valid email.');
+                    }
+                  }
+                },
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style :TextStyle(color:Colors.red,fontSize: 14.0),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
