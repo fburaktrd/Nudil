6c57f992e4668a4383559849afbe6de9a0aa2611
fburaktrd <fburakturedi@hotmail.com>
2021-04-07 04:59:48 +0300

    User sign in and up added

---
+++ android/app/src/main/kotlin/com/example/nyp_proje/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nyp_proje
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_first_app/models/user.dart';
+import 'package:flutter_first_app/screens/wrapper.dart';
+import 'package:flutter_first_app/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'models/user.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
+}
