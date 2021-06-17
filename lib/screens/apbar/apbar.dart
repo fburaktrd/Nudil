2a3a99648f095214a0d8cf0b1cc85fb6e74b474c
ibrahim cinar <cinar-ibrahim@outlook.com>
2021-06-17 15:41:15 +0300

    Revert "Revert "Merge branch 'son-deneme-yeni-hal' of https://github.com/fburaktrd/nyp-proje into son-deneme-yeni-hal""
    This reverts commit 3b27aeb942e63fc7384f3e26c85efe5e9ba77fa0.

--- lib/screens/apbar/apbar.dart
+++ lib/screens/apbar/apbar.dart
@@ -1,50 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_first_app/main.dart';
+import 'package:flutter_first_app/models/database.dart';
 import 'package:flutter_first_app/services/auth.dart';
 
 import 'arkadasEkle.dart';
 
 final AuthService _auth = AuthService();
+
 class Apbar {
   final BuildContext context;
   final Widget widget;
-  Apbar({this.context,this.widget});
+  Apbar({this.context, this.widget});
 
-
-
-  AppBar x(){
+  AppBar x() {
     return AppBar(
-      automaticallyImplyLeading: false,
-      title: Text("Nudıl"),
-      backgroundColor: Color(0xff30374b),
-      actions:[
-
-        IconButton(
-          
-          icon: Icon(Icons.person_add_alt_1),
-          onPressed: () {
-            widget.key.toString()=="[<'asd'>]"?Navigator.pop(context):Navigator.push(context, MaterialPageRoute(builder: (context)=>RestartWidget(child: ArkadasEkle())));
-            
-          }
-        ),
-        IconButton(
-          icon: Icon(Icons.logout),
-          onPressed: () async{
-            await _auth.signOut();
-            final snackBar = SnackBar(
-                backgroundColor: Colors.lightBlue,
-                content: Text(
-                  "Başarıyla çıkış yapıldı , görüşmek üzere !",
-                  style: TextStyle(fontSize: 20),
-                ));
-            ScaffoldMessenger.of(context)
-                .showSnackBar(snackBar);
-            
-          }
-        ),
-      ]
-    );
-
+        automaticallyImplyLeading: false,
+        title: Text("Nudıl"),
+        backgroundColor: Color(0xff30374b),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.person_add_alt_1),
+              onPressed: () {
+                widget.key.toString() == "[<'asd'>]"
+                    ? Navigator.pop(context)
+                    : Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                RestartWidget(child: ArkadasEkle())));
+              }),
+          IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: () async {
+                String uid = await _auth.getUseruid();
+                String userName =
+                    await DataBaseConnection.getUserDisplayName(uid);
+                await _auth.signOut();
+                final snackBar = SnackBar(
+                    backgroundColor: Colors.lightBlue,
+                    content: Text(
+                      "Başarıyla çıkış yapıldı , görüşmek üzere $userName !",
+                      style: TextStyle(fontSize: 20),
+                    ));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }),
+        ]);
   }
-  
 }
